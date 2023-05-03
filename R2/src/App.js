9fb6b82fe6e81400a1a47e490aa2c50126bccb7f
Zapata-Diallo <alphaloppecity@gmail.com>
2023-05-03 19:28:47 +0000

    checkpointRedux

---
+++ R2/src/App.js
@@ -0,0 +1,24 @@
+
+import "./styles.css";
+import { Provider } from "react-redux";
+import TaskForm from "./TaskForm";
+import TasksHeader from "./TasksHeader";
+import TasksList from "./TasksList";
+import { store } from "./Redux";
+
+export default function App() {
+
+  return (
+    <Provider store={store}>
+    <div className="container">
+      <article>
+        <TasksHeader />
+        <TasksList />
+        <footer>
+          <TaskForm />
+        </footer>
+      </article>
+    </div>
+    </Provider>
+  );
+}
